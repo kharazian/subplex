0b826d5974e791f677374ddfcb5c4e67085e9a80
kharazian <amirkharazian@gmail.com>
2019-09-22 14:28:47 +0330

    some change

--- hello.f
+++ hello.f
@@ -3,16 +3,24 @@
             double precision x(n)
             integer i
             double precision sum
-            sum = x(1)**2 + x(2)**2
+            sum = 0
+            do 10 i = 1,n
+            sum = sum + x(i)**2
+10      continue
             f = sum
             RETURN
             end
         
         program hello
-            integer n,maxnfe,nfe,iwork(4),iflag
+            integer n, maxnfe,nfe,iwork(2*2),iflag
             double precision tol,scale(2),x(2),fx,work(17)
             external f
+            
             n = 2
+            ! call alloca_fortran(iwork, n, TYPE_INT) ! Allocate 20 integers for a
+
+            ! call alloca_fortran(b, 14, TYPE_DBL) ! Allocate 14 doubles for b
+
             tol = 0.00000000000001
             x = (/11 , -33/)
             scale = -1
